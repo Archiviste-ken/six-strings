04179a360c367c471b79918edbf6142eecc4ecff
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-27 12:48:06 +0530

    feat: add Band interface to define band structure

---
+++ types/band.ts
@@ -0,0 +1,15 @@
+export interface Band {
+  id: number;
+  name: string;
+  slug: string;
+
+  image: string;
+
+  genre: string;
+
+  country: string;
+
+  formed: number;
+
+  description: string;
+}
