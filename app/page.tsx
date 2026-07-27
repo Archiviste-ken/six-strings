652ffceced839ec31d300cc9de874d6d1e83fe93
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-27 13:55:53 +0530

    feat: add FeaturedBands and BrowseGenres components to HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import FeaturedBands from "@/src/components/bands/FeaturedBands";
+import BrowseGenres from "@/src/components/genres/BrowseGenres";
 import Hero from "@/src/components/home/Hero";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <FeaturedBands />
+      <BrowseGenres />
     </>
   );
 }
