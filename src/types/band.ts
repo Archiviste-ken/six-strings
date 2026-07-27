8a9ded1e935ad01a8d2f0b83eafb83c5c2764b17
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-27 15:21:30 +0530

    feat: update Band interface to include history and ensure proper formatting

--- src/types/band.ts
+++ src/types/band.ts
@@ -2,14 +2,16 @@ export interface Band {
   id: number;
   name: string;
   slug: string;
-
   image: string;
 
   genre: string;
-
   country: string;
-
   formed: number;
 
   description: string;
+  history: string;
+
+  members: string[];
+
+  albums: number[];
 }
