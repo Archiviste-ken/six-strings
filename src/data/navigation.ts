47a06728acefed5441020b7df134d0d9b7a39cc8
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-27 13:57:42 +0530

    feat: add navigation links for Bands, Albums, Favorites, and About

---
+++ src/data/navigation.ts
@@ -0,0 +1,18 @@
+export const navigationLinks = [
+  {
+    name: "Bands",
+    href: "/bands",
+  },
+  {
+    name: "Albums",
+    href: "/albums",
+  },
+  {
+    name: "Favorites",
+    href: "/favorites",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+];
