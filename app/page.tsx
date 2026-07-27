8897e1eedea3d470ffba3259706ca5419d48f829
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-27 14:27:04 +0530

    chore: add a blank line for improved code readability in HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import FeaturedBands from "@/src/components/bands/FeaturedBands";
 import BrowseGenres from "@/src/components/genres/BrowseGenres";
 import Hero from "@/src/components/home/Hero";
 
+
 export default function HomePage() {
   return (
     <>
